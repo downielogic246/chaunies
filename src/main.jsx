64657c350bae2b33392bdbcfe278afc3fc4bb56c
mardanca <124599569+mardanca@users.noreply.github.com>
2023-06-16 15:32:53 -0700

    yeah

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/chaunies.io">
       <App />
     </BrowserRouter>
   </React.StrictMode>
